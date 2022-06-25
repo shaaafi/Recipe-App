06825ff9bcecf2b6b43d9bf63c984d474af0b0b6
shaaafi <shafiul2344@gmail.com>
2022-06-25 22:58:44 +0600

    1 file modified

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <h3>Trying a recepe app on react with StAcKBliTz</h3>
+      <h3>Trying a recipe app on react with StAcKBliTz</h3>
     </div>
   );
 }
